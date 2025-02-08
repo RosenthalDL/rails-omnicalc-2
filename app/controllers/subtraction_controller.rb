6cd4c8778ca6e6c3a2be5ef8f93476dd2ed229a2
Danielle Rosenthal <danieller530@gmail.com>
2025-02-08 23:04:54 +0000

    adding latest code

--- app/controllers/subtraction_controller.rb
+++ app/controllers/subtraction_controller.rb
@@ -1,4 +1,4 @@
-class subtraction_controller < ApplicationController
+class SubtractionController < ApplicationController
   def show_subtraction_form
     render({ :template => "subtraction_templates/subtraction_form" })
   end
@@ -9,6 +9,6 @@ class subtraction_controller < ApplicationController
 
     @result =  @second_number - @first_number
 
-    render({ :template => "subtraction_templates/subtract_results" })
+    render({ :template => "subtraction_templates/sub_results" })
   end
 end
